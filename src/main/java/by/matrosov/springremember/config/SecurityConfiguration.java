87fde69a8f3d3c2d45d7975a2d18367af14dbe63
Andrey Matrosov <matrosovs2008@yandex.ru>
2018-06-19 18:10:39 +0300

    impl configure methods in security config

--- src/main/java/by/matrosov/springremember/config/SecurityConfiguration.java
+++ src/main/java/by/matrosov/springremember/config/SecurityConfiguration.java
@@ -1,10 +1,10 @@
 package by.matrosov.springremember.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -28,7 +28,25 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.jdbcAuthentication()
+                .usersByUsernameQuery("select username, password, enabled from users where username=?")
+                .authoritiesByUsernameQuery("select u.username, r.role_name from users u" +
+                        " inner join" +
+                        " users_roles ur on u.user_id = ur.user_id" +
+                        " inner join roles r on ur.role_id = r.role_id" +
+                        " where u.username=?")
                 .dataSource(dataSource)
                 .passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/login").permitAll()
+                .antMatchers("/hello").hasAnyAuthority("ADMIN", "USER", "MODERATOR")
+                .and()
+                .formLogin().loginPage("/login").defaultSuccessUrl("/hello")
+                .and()
+                .csrf().disable();
     }
 }
--- src/main/java/by/matrosov/springremember/controller/UserController.java
+++ src/main/java/by/matrosov/springremember/controller/UserController.java
@@ -3,12 +3,20 @@ package by.matrosov.springremember.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class UserController {
 
-    @RequestMapping(method = RequestMethod.GET)
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String hello(){
         return "hello";
+    }
+
+    @RequestMapping(value={"/", "/login"}, method = RequestMethod.GET)
+    public ModelAndView login(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("login");
+        return modelAndView;
     }
 }
